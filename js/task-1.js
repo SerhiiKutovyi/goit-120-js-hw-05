5eb44a221074df7b2ff53a5fc8a9d5dce6ed23ad
SerhiiKutovyi <wagenrex@gmail.com>
2025-08-04 18:07:42 +0300

    hw-1

--- js/task-1.js
+++ js/task-1.js
@@ -107,16 +107,64 @@
 // }
 // console.log(filterArray([1, 2, 3, 4, 5], 3));
 
-const filterArray = (numbers, value) => {
-  const filteredNumbers = [];
+// const filterArray = (numbers, value) => {
+//   const filteredNumbers = [];
 
-  numbers.forEach(number => {
-    if (number > value) {
-      filteredNumbers.push(number);
-    }
-  });
+//   numbers.forEach(number => {
+//     if (number > value) {
+//       filteredNumbers.push(number);
+//     }
+//   });
 
-  return filteredNumbers;
-};
+//   return filteredNumbers;
+// };
 
-console.log(filterArray([1, 2, 3, 4, 5], 3));
+// console.log(filterArray([1, 2, 3, 4, 5], 3));
+
+// const dirtyMultiply = (array, value) => {
+//   for (let i = 0; i < array.length; i += 1) {
+//     array[i] = array[i] * value;
+//   }
+// };
+
+// const numbers = [1, 2, 3, 4, 5];
+// dirtyMultiply(numbers, 2);
+// // Відбулася мутація вихідних даних - масиву numbers
+// console.log(numbers); // [2, 4, 6, 8, 10]
+
+// const pureMultiply = (array, value) => {
+//   const newArray = [];
+
+//   array.forEach(element => {
+//     newArray.push(element * value);
+//   });
+
+//   return newArray;
+// };
+
+// const numbers = [1, 2, 3, 4, 5];
+// pureMultiply(numbers, 2);
+
+// console.log(pureMultiply(numbers, 2));
+
+// console.log(numbers);
+
+// function changeEven(numbers, value) {
+//   const newArray = [];
+//   numbers.forEach(element => {
+//     if (element % 2 === 0) {
+//       newArray.push((element += value));
+//     } else {
+//       newArray.push(element);
+//     }
+//   });
+//   return newArray;
+// }
+
+// console.log(changeEven([17, 24, 68, 31, 42], 100));
+
+const planets = ['Earth', 'Mars', 'Venus', 'Jupiter'];
+
+const planetsLengths = planets.map(ele => ele.length);
+
+console.log(planetsLengths);

e252398eac76d6fdd766314a69f2f833e6948a2a
SerhiiKutovyi <wagenrex@gmail.com>
2025-08-08 14:52:26 +0300

    hw-3

--- js/task-2.js
+++ js/task-2.js
@@ -32,4 +32,4 @@ const allUsers = [
 const getUsersWithFriend = (users, friendName) =>
   users.filter(user => user.friends.includes(friendName));
 
-console.log(getUsersWithFriend(allUsers, 'Briana Decker'));
+console.log(getUsersWithFriend(allUsers, 'Goldie Gentry'));
